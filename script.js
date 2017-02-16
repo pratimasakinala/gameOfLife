e926e038d444d41f9b17c0f64b73442b4952a01e
pratimasakinala <pratimasakinala13@gmail.com>
2017-02-16 15:47:47 -0800

    adds glider gun. color schemes

--- script.js
+++ script.js
@@ -1,103 +1,169 @@
-var tableHolder = document.getElementById('table');
+var tableHolder = document.getElementById('table'),
+  gun = document.querySelectorAll('button')[0],
+  random = document.querySelectorAll('button')[1],
+  reset = document.querySelectorAll('button')[2],
+  color = document.querySelectorAll('button')[3],
+  interval,
+  isGosperGun = false,
+  gridHeight = 70,
+  gridWidth = 120;
+
+var grid = createGrid(gridHeight, gridWidth);
 
 // Create a grid and randomly assign 1 or 0 to each cell
-var grid = function (rows) {
+function createGrid(rows, cols) {
   var gridView = document.createElement('table'),
     arr = [];
   tableHolder.appendChild(gridView);
+  tableHolder.firstChild.classList.add('color-scheme1');
 
-  for (var i = 0; i<rows; i++) {
+  for (var i = 0; i < rows; i++) {
     arr[i] = [];
     gridView.insertRow(i);
 
-    for (var j=0; j < rows; j++) {
+    for (var j = 0; j < cols; j++) {
       gridView.firstChild.childNodes[i].insertCell(j);
-      arr[i][j] = ( Math.round(Math.random()) ) ? 1 : 0;
+      arr[i][j] = 0;
     }
   }
-
-  // arr[1][26] = 1;
-  // arr[2][24] = arr[2][26] = 1;
-  // arr[3][14] = arr[3][15] = arr[3][22] = arr[3][23] = arr[3][36] = arr[3][37] = 1;
-  // arr[4][13] = arr[4][17] = arr[4][22] = arr[4][23] = arr[4][36] = arr[4][37] = 1;
-  // arr[5][1] = arr[5][2] = arr[5][12] = arr[5][18] = arr[4][22] = arr[4][23] = 1;
-  // arr[6][1] = arr[6][2] = arr[5][12] = arr[5][16] = arr[5][18] = arr[5][19] = arr[5][24] = arr[5][26] = 1;
-  // arr[7][12] = arr[7][18] = arr[5][26] = 1;
-  // arr[8][13] = arr[8][17] = 1;
-  // arr[9][14] = arr[9][15] = 1;
-
   return arr;
-}(40);
+}
 
 // Show the grid on the window
 function drawGrid() {
   grid.forEach(function(row, index) {
     row.forEach(function(cell, cellIndex) {
-      if (cell) tableHolder.firstChild.firstChild.childNodes[index].childNodes[cellIndex].classList.add('live');
-      else tableHolder.firstChild.firstChild.childNodes[index].childNodes[cellIndex].classList.remove('live');
+      if (cell) {
+          tableHolder.firstChild.firstChild.childNodes[index].childNodes[cellIndex].classList.remove('was-live');
+          tableHolder.firstChild.firstChild.childNodes[index].childNodes[cellIndex].classList.add('live');
+      } else {
+          if (tableHolder.firstChild.firstChild.childNodes[index].childNodes[cellIndex].classList.contains('live')) {
+            tableHolder.firstChild.firstChild.childNodes[index].childNodes[cellIndex].classList.remove('live');
+            tableHolder.firstChild.firstChild.childNodes[index].childNodes[cellIndex].classList.add('was-live');
+          }
+      }
     })
   });
 }
 
+function clearGrid() {
+  tableHolder.removeChild(tableHolder.childNodes[0]);
+  clearInterval(interval);
+  createGrid(gridHeight, gridWidth);
+}
+
+// Find neighbors
 function checkNeighbors(index, cellIndex) {
   var totalNeighbors = 0,
     position = 1,
     newIndex = index,
     cellIndex = cellIndex;
 
-    while(position < 9) {
-      switch (position) {
-        case 1:
+  // console.log({isGosperGun});
+  while(position < 9) {
+    switch (position) {
+      case 1:
+        if (isGosperGun) {
+          newIndex = index - 1;
+          newCellIndex = cellIndex - 1;
+        } else {
           // If current row is the first row then check top left neighbor in last row
-          newIndex = (index === 0) ? (grid.length - 1) : (index - 1);
+          newIndex = (index === 0) ? (gridHeight - 1) : (index - 1);
           // If current cell is first column then check top left neighbor in last column
-          newCellIndex = (cellIndex === 0) ? (grid.length - 1) : cellIndex - 1;
-          break;
-        case 2:
-          // If current row is the first row then check top center neighbor in last row
-          newIndex = (index === 0) ? (grid.length - 1) : (index - 1);
-          newCellIndex = cellIndex;
-          break;
-        case 3:
-          // If current row is the first row then check top right neighbor in last row
-          newIndex = (index === 0) ? (grid.length - 1) : (index - 1);
-          // If current cell is last column then check top right neighbor in first column
-          newCellIndex = (cellIndex === grid.length - 1) ? 0 : (cellIndex + 1);
-          break;
-        case 4:
-          newIndex = index;
-          // If current cell is first column then check left neighbor in last column
-          newCellIndex = (cellIndex === 0) ? (grid.length - 1) : (cellIndex - 1);
-          break;
-        case 5:
-          newIndex = index;
-          // If current cell is last column then check right neighbor in first column
-          newCellIndex = (cellIndex === grid.length - 1) ? 0 : (cellIndex + 1);
-          break;
-        case 6:
-          // If current row is the last row then check bottom left neighbor in first row
-          newIndex = (index === grid.length - 1) ? 0 : (index + 1);
-          // If current cell is first column then check bottom left neighbor in last column
-          newCellIndex = (cellIndex === 0) ? (grid.length - 1) : (cellIndex - 1);
-          break;
-        case 7:
-          // If current row is the last row then check bottom center neighbor in first row
-          newIndex = (index === grid.length - 1) ? 0 : (index + 1);
-          newCellIndex = cellIndex;
-          break;
-        case 8:
-          // If current row is the last row then check bottom center neighbor in first row
-          newIndex = (index === grid.length - 1) ? 0 : (index + 1);
-          // If current cell is last column then check bottom left neighbor in first column
-          newCellIndex = (cellIndex === grid.length - 1) ? 0 : (cellIndex + 1);
-          break;
-      }
+          newCellIndex = (cellIndex === 0) ? (gridWidth - 1) : cellIndex - 1;
+        }
 
-      // console.log({position, newIndex, newCellIndex});
-      totalNeighbors += grid[newIndex][newCellIndex];
-      position++;
+        break;
+      case 2:
+        if (isGosperGun) {
+          newIndex = index - 1;
+        } else {
+          // If current row is the first row then check top center neighbor in last row
+          newIndex = (index === 0) ? (gridHeight - 1) : (index - 1);
+        }
+        newCellIndex = cellIndex;
+
+        break;
+      case 3:
+        if (isGosperGun) {
+          newIndex = index - 1;
+          newCellIndex = cellIndex + 1;
+        } else {
+          // If current row is the first row then check top right neighbor in last row
+          newIndex = (index === 0) ? (gridHeight - 1) : (index - 1);
+          // If current cell is last column then check top right neighbor in first column
+          newCellIndex = (cellIndex === gridWidth - 1) ? 0 : (cellIndex + 1);
+        }
+
+        break;
+      case 4:
+        newIndex = index;
+
+        if (isGosperGun) {
+          newCellIndex = cellIndex - 1;
+        } else {
+          // If current cell is first column then check left neighbor in last column
+          newCellIndex = (cellIndex === 0) ? (gridWidth - 1) : (cellIndex - 1);
+        }
+
+        break;
+      case 5:
+        newIndex = index;
+
+        if (isGosperGun) {
+          newCellIndex = cellIndex + 1;
+        } else {
+          // If current cell is last column then check right neighbor in first column
+          newCellIndex = (cellIndex === gridWidth - 1) ? 0 : (cellIndex + 1);
+        }
+
+        break;
+      case 6:
+        if (isGosperGun) {
+          newIndex = index + 1;
+          newCellIndex = cellIndex - 1;
+        } else {
+          // If current row is the last row then check bottom left neighbor in first row
+          newIndex = (index === gridHeight - 1) ? 0 : (index + 1);
+          // If current cell is first column then check bottom left neighbor in last column
+          newCellIndex = (cellIndex === 0) ? (gridWidth - 1) : (cellIndex - 1);
+        }
+
+        break;
+      case 7:
+        if (isGosperGun) {
+          newIndex = index + 1;
+        } else {
+          // If current row is the last row then check bottom center neighbor in first row
+          newIndex = (index === gridHeight - 1) ? 0 : (index + 1);
+        }
+        newCellIndex = cellIndex;
+
+        break;
+      case 8:
+        if (isGosperGun) {
+          newIndex = index + 1;
+          newCellIndex = cellIndex + 1;
+        } else {
+          // If current row is the last row then check bottom center neighbor in first row
+          newIndex = (index === gridHeight - 1) ? 0 : (index + 1);
+          // If current cell is last column then check bottom left neighbor in first column
+          newCellIndex = (cellIndex === gridWidth - 1) ? 0 : (cellIndex + 1);
+        }
+
+        break;
     }
-    return totalNeighbors;
+
+    // console.log({position, newIndex, newCellIndex});
+    if (newIndex >= 0 && newIndex < gridHeight && newCellIndex >= 0 && newCellIndex < gridWidth) {
+      // console.log("valid indices");
+      // if (grid[newIndex][newCellIndex]) console.log("found a live neighbor");
+      // else console.log("dead neighbor");
+      totalNeighbors += grid[newIndex][newCellIndex];
+    }
+    position++;
+  }
+  return totalNeighbors;
 }
 
 // Check if cell is alive
@@ -156,13 +222,63 @@ function checkIfAlive() {
 
   // console.log("updating grid");
   grid = updateGrid;
+  drawGrid();
 }
 
 drawGrid();
-setInterval(loop, 200);
 
 function loop() {
   checkIfAlive();
   drawGrid();
   // requestAnimationFrame(loop);
 }
+
+// Create gosper's glider gun
+gun.addEventListener('click', function() {
+  isGosperGun = true;
+
+  grid[1][25] = 1;
+  grid[2][23] = grid[2][25] = 1;
+  grid[3][13] = grid[3][14] = grid[3][21] = grid[3][22] = grid[3][35] = grid[3][36] = 1;
+  grid[4][12] = grid[4][16] = grid[4][21] = grid[4][22] = grid[4][35] = grid[4][36] = 1;
+  grid[5][1] = grid[5][2] = grid[5][11] = grid[5][17] = grid[5][21] = grid[5][22] = 1;
+  grid[6][1] = grid[6][2] = grid[6][11] = grid[6][15] = grid[6][17] = grid[6][18] = grid[6][23] = grid[6][25] = 1;
+  grid[7][11] = grid[7][17] = grid[7][25] = 1;
+  grid[8][12] = grid[8][16] = 1;
+  grid[9][13] = grid[9][14] = 1;
+
+  drawGrid();
+  interval = setInterval(loop, 200);
+});
+
+// Create a random pattern
+random.addEventListener('click', function() {
+  isGosperGun = false;
+  clearGrid();
+
+  grid.forEach(function(row, i) {
+    row.forEach(function(cell, j) {
+        grid[i][j] = Math.round(Math.random());
+    });
+  });
+
+  drawGrid();
+  interval = setInterval(loop, 200);
+});
+
+reset.addEventListener('click', function() {
+  clearGrid();
+});
+
+color.addEventListener('click', function() {
+  if (tableHolder.firstChild.classList.contains('color-scheme1')) {
+    tableHolder.firstChild.classList.remove('color-scheme1');
+    tableHolder.firstChild.classList.add('color-scheme2');
+  } else if (tableHolder.firstChild.classList.contains('color-scheme2')) {
+    tableHolder.firstChild.classList.remove('color-scheme2');
+    tableHolder.firstChild.classList.add('color-scheme3');
+  } else if (tableHolder.firstChild.classList.contains('color-scheme3')) {
+    tableHolder.firstChild.classList.remove('color-scheme3');
+    tableHolder.firstChild.classList.add('color-scheme1');
+  }
+});
